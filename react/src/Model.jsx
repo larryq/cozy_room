d46eea2be711769b5d7f1385e914d273851cc4eb
Larry Quantz <lquantz@gmail.com>
2025-11-03 13:49:43 -0800

    denoised baked image

--- react/src/Model.jsx
+++ react/src/Model.jsx
@@ -2,7 +2,7 @@ import { useGLTF } from "@react-three/drei";
 import { useEffect } from "react";
 
 export default function Model({ onLoaded }) {
-  const { scene } = useGLTF("/assets/cozy_room_jpg.glb");
+  const { scene } = useGLTF("/assets/cozy_room_jpeg_denoised.glb");
 
   return <primitive object={scene} />;
 }
